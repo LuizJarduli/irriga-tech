8c086147badba052b7345695fd43022ad685cd6b
Luiz Jarduli <luizjarduli@gmail.com>
2025-10-24 22:03:18 -0300

    chore: format configuration files for consistency
    - Ensure proper indentation and formatting in astro.config.mjs and vercel.json
    - Add missing newline at the end of the files

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: "https://irrigatech.vercel.app",
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: { enabled: true },
   }),
   integrations: [mdx(), sitemap(), icon()],
   vite: {
